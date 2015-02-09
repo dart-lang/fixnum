2863f180bdbae377c0adfa876890094de07afa2c
Chris Bracken <chris@bracken.jp>
2015-02-09 00:07:58 -0800

    Add Travis CI configuration.

---
+++ tool/travis.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Copyright (c) 2015, the Dart project authors. Please see the AUTHORS file for
+# details. All rights reserved. Use of this source code is governed by a
+# BSD-style license that can be found in the LICENSE file.
+
+# Fast fail the script on failures.
+set -e
+
+# Verify that the libraries are error and warning-free.
+echo "Running dartanalyzer..."
+dartanalyzer lib/fixnum.dart test/all_tests.dart
+
+# Run the tests.
+echo "Running tests..."
+dart -c test/all_tests.dart
