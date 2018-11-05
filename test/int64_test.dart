fa660439d4d53387f6ef4ab07a01233ce69a7f85
Stephen Adams <sra@google.com>
2018-11-04 20:37:38 -0800

    Update int64_test.dart

--- test/int64_test.dart
+++ test/int64_test.dart
@@ -888,5 +888,17 @@ void main() {
       expect(() => new Int64(42).toRadixString(0), throwsArgumentError);
       expect(() => new Int64(42).toRadixString(37), throwsArgumentError);
     });
+    
+    test("toStringUnsigned", () {
+      expect(Int64.ZERO.toStringUnsigned(), "0");
+      for (int high5 = 0; high5 < 32; high5++) {
+        for (int low2 = 0; low2 < 4; low2++) {
+          Int64 value = (new Int64(high5) << (64 - 5)) + new Int64(low2);
+          String s = value.toStringUnsigned();
+          Int64 value2 = Int64.parseRadix(s, 10);
+          expect(value2, value)l
+        }
+      }
+    });
   });
 }
