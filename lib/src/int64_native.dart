71b813bff7ab2d8d971f0c8e1aab0d8122fa3f55
Ömer Sinan Ağacan <omersa@google.com>
2023-10-17 09:36:29 +0200

    Implement `Int64` as a wrapper around `int`
    On AOT, JIT, and Wasm we don't need to emulate 64-bit integers as the
    `int` type is already 64 bits.
    
    This PR adds another implementation of `Int64` that is just a wrapper
    around `int`:
    
        class Int64 implements IntX {
          final int _i;
          ...
        }
    
    `int64` library imports this implementation in VM and dart2wasm, and the
    old (emulated) `Int64` implementation on dart2js.
    
    The conditional import is implemented based on availability of the
    library `dart:html`, which is only available on dart2js.
    
    (In principle dart2wasm could support `dart:html` as well, but it won't
    be supported, instead dart2wasm will support `package:web`)
    
    Documentations of `Int64` and its members are copied directly from the
    old emulated `Int64` class to make sure the documentation will look the
    same as before.
    
    The old `toRadixStringUnsigned` implementation is moved to the utilities
    library and reused in the new class.

---
+++ lib/src/int64_native.dart
@@ -0,0 +1,374 @@
+// Copyright (c) 2023, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'int32.dart';
+import 'intx.dart';
+import 'utilities.dart' as u;
+
+/// An immutable 64-bit signed integer, in the range [-2^63, 2^63 - 1].
+/// Arithmetic operations may overflow in order to maintain this range.
+class Int64 implements IntX {
+  final int _i;
+
+  /// The maximum positive value attainable by an [Int64], namely
+  /// 9,223,372,036,854,775,807.
+  static const Int64 MAX_VALUE = Int64(9223372036854775807);
+
+  /// The minimum positive value attainable by an [Int64], namely
+  /// -9,223,372,036,854,775,808.
+  static const Int64 MIN_VALUE = Int64(-9223372036854775808);
+
+  /// An [Int64] constant equal to 0.
+  static const Int64 ZERO = Int64(0);
+
+  /// An [Int64] constant equal to 1.
+  static const Int64 ONE = Int64(1);
+
+  /// An [Int64] constant equal to 2.
+  static const Int64 TWO = Int64(2);
+
+  /// Constructs an [Int64] with a given [int] value; zero by default.
+  const Int64([int value = 0]) : _i = value;
+
+  /// Constructs an [Int64] from a pair of 32-bit integers having the value
+  /// [:((top & 0xffffffff) << 32) | (bottom & 0xffffffff):].
+  factory Int64.fromInts(int top, int bottom) =>
+      Int64((top << 32) | (bottom & 0xFFFFFFFF));
+
+  factory Int64.fromBytes(List<int> bytes) => Int64(((bytes[7] & 0xFF) << 56) |
+      ((bytes[6] & 0xFF) << 48) |
+      ((bytes[5] & 0xFF) << 40) |
+      ((bytes[4] & 0xFF) << 32) |
+      ((bytes[3] & 0xFF) << 24) |
+      ((bytes[2] & 0xFF) << 16) |
+      ((bytes[1] & 0xFF) << 8) |
+      (bytes[0] & 0xFF));
+
+  factory Int64.fromBytesBigEndian(List<int> bytes) =>
+      Int64(((bytes[0] & 0xFF) << 56) |
+          ((bytes[1] & 0xFF) << 48) |
+          ((bytes[2] & 0xFF) << 40) |
+          ((bytes[3] & 0xFF) << 32) |
+          ((bytes[4] & 0xFF) << 24) |
+          ((bytes[5] & 0xFF) << 16) |
+          ((bytes[6] & 0xFF) << 8) |
+          (bytes[7] & 0xFF));
+
+  /// Parses [source] as a decimal numeral.
+  ///
+  /// Returns an [Int64] with the numerical value of [source]. If the numerical
+  /// value of [source] does not fit in a signed 64 bit integer, the numerical
+  /// value is truncated to the lowest 64 bits of the value's binary
+  /// representation, interpreted as a 64-bit two's complement integer.
+  ///
+  /// The [source] string must contain a sequence of digits (`0`-`9`), possibly
+  /// prefixed by a `-` sign.
+  ///
+  /// Throws a [FormatException] if the input is not a valid decimal integer
+  /// numeral.
+  static Int64 parseInt(String source) => _parseRadix(source, 10, true)!;
+
+  /// Parses [source] as a decimal numeral.
+  ///
+  /// Returns an [Int64] with the numerical value of [source]. If the numerical
+  /// value of [source] does not fit in a signed 64 bit integer, the numerical
+  /// value is truncated to the lowest 64 bits of the value's binary
+  /// representation, interpreted as a 64-bit two's complement integer.
+  ///
+  /// The [source] string must contain a sequence of digits (`0`-`9`), possibly
+  /// prefixed by a `-` sign.
+  ///
+  /// Returns `null` if the input is not a valid decimal integer numeral.
+  static Int64? tryParseInt(String source) => _parseRadix(source, 10, false);
+
+  /// Parses [source] in a given [radix] between 2 and 36.
+  ///
+  /// Returns an [Int64] with the numerical value of [source]. If the numerical
+  /// value of [source] does not fit in a signed 64 bit integer, the numerical
+  /// value is truncated to the lowest 64 bits of the value's binary
+  /// representation, interpreted as a 64-bit two's complement integer.
+  ///
+  /// The [source] string must contain a sequence of base-[radix] digits (using
+  /// letters from `a` to `z` as digits with values 10 through 25 for radixes
+  /// above 10), possibly prefixed by a `-` sign.
+  ///
+  /// Throws a [FormatException] if the input is not recognized as a valid
+  /// integer numeral.
+  static Int64 parseRadix(String source, int radix) =>
+      _parseRadix(source, u.validateRadix(radix), true)!;
+
+  /// Parses [source] in a given [radix] between 2 and 36.
+  ///
+  /// Returns an [Int64] with the numerical value of [source]. If the numerical
+  /// value of [source] does not fit in a signed 64 bit integer, the numerical
+  /// value is truncated to the lowest 64 bits of the value's binary
+  /// representation, interpreted as a 64-bit two's complement integer.
+  ///
+  /// The [source] string must contain a sequence of base-[radix] digits (using
+  /// letters from `a` to `z` as digits with values 10 through 25 for radixes
+  /// above 10), possibly prefixed by a `-` sign.
+  ///
+  /// Returns `null` if the input is not recognized as a valid integer numeral.
+  static Int64? tryParseRadix(String source, int radix) =>
+      _parseRadix(source, u.validateRadix(radix), false);
+
+  /// Parses [source] as a hexadecimal numeral.
+  ///
+  /// Returns an [Int64] with the numerical value of [source]. If the numerical
+  /// value of [source] does not fit in a signed 64 bit integer, the numerical
+  /// value is truncated to the lowest 64 bits of the value's binary
+  /// representation, interpreted as a 64-bit two's complement integer.
+  ///
+  /// The [source] string must contain a sequence of hexadecimal digits
+  /// (`0`-`9`, `a`-`f` or `A`-`F`), possibly prefixed by a `-` sign.
+  ///
+  /// Throws a [FormatException] if the input is not a valid hexadecimal
+  /// integer numeral.
+  static Int64 parseHex(String source) => _parseRadix(source, 16, true)!;
+
+  /// Parses [source] as a hexadecimal numeral.
+  ///
+  /// Returns an [Int64] with the numerical value of [source].
+  /// If the numerical value of [source] does not fit
+  /// in a signed 64 bit integer,
+  /// the numerical value is truncated to the lowest 64 bits
+  /// of the value's binary representation,
+  /// interpreted as a 64-bit two's complement integer.
+  ///
+  /// The [source] string must contain a sequence of hexadecimal
+  /// digits (`0`-`9`, `a`-`f` or `A`-`F`), possibly prefixed by a `-` sign.
+  ///
+  /// Returns `null` if the input is not a valid
+  /// hexadecimal integer numeral.
+  static Int64? tryParseHex(String source) => _parseRadix(source, 16, false);
+
+  static Int64? _parseRadix(String s, int radix, bool throwOnError) {
+    int charIdx = 0;
+    bool negative = false;
+    if (s.startsWith('-')) {
+      negative = true;
+      charIdx++;
+    }
+
+    if (charIdx >= s.length) {
+      if (!throwOnError) return null;
+      throw FormatException('No digits', s, charIdx);
+    }
+
+    int i = 0;
+    for (; charIdx < s.length; charIdx++) {
+      int c = s.codeUnitAt(charIdx);
+      int digit = u.decodeDigit(c);
+      if (digit < radix) {
+        i = (i * radix) + digit;
+      } else {
+        if (!throwOnError) return null;
+        throw FormatException('Not radix digit', s, charIdx);
+      }
+    }
+
+    if (negative) {
+      return Int64(-i);
+    }
+
+    return Int64(i);
+  }
+
+  static int _promote(Object value) {
+    if (value is Int64) {
+      return value._i;
+    } else if (value is int) {
+      return value;
+    } else if (value is Int32) {
+      return value.toInt();
+    }
+    throw ArgumentError.value(value, 'other', 'not an int, Int32 or Int64');
+  }
+
+  @override
+  Int64 operator +(Object other) => Int64(_i + _promote(other));
+
+  @override
+  Int64 operator -(Object other) => Int64(_i - _promote(other));
+
+  @override
+  Int64 operator -() => Int64(-_i);
+
+  @override
+  Int64 operator *(Object other) => Int64(_i * _promote(other));
+
+  @override
+  Int64 operator %(Object other) => Int64(_i % _promote(other));
+
+  @override
+  Int64 operator ~/(Object other) => Int64(_i ~/ _promote(other));
+
+  @override
+  Int64 remainder(Object other) => Int64(_i.remainder(_promote(other)));
+
+  @override
+  Int64 operator &(Object other) => Int64(_i & _promote(other));
+
+  @override
+  Int64 operator |(Object other) => Int64(_i | _promote(other));
+
+  @override
+  Int64 operator ^(Object other) => Int64(_i ^ _promote(other));
+
+  @override
+  Int64 operator ~() => Int64(~_i);
+
+  @override
+  Int64 operator <<(int shiftAmount) => Int64(_i << shiftAmount);
+
+  @override
+  Int64 operator >>(int shiftAmount) => Int64(_i >> shiftAmount);
+
+  @override
+  Int64 shiftRightUnsigned(int shiftAmount) => Int64(_i >>> shiftAmount);
+
+  @override
+  int compareTo(Object other) => _i.compareTo(_promote(other));
+
+  @override
+  bool operator <(Object other) => _i < _promote(other);
+
+  @override
+  bool operator <=(Object other) => _i <= _promote(other);
+
+  @override
+  bool operator >(Object other) => _i > _promote(other);
+
+  @override
+  bool operator >=(Object other) => _i >= _promote(other);
+
+  @override
+  bool get isEven => _i.isEven;
+
+  @override
+  bool get isMaxValue => this == MAX_VALUE;
+
+  @override
+  bool get isMinValue => this == MIN_VALUE;
+
+  @override
+  bool get isNegative => _i < 0;
+
+  @override
+  bool get isOdd => _i.isOdd;
+
+  @override
+  bool get isZero => _i == 0;
+
+  @override
+  int get bitLength => _i.bitLength;
+
+  /// Returns a hash code based on all the bits of this [Int64].
+  @override
+  int get hashCode => _i.hashCode;
+
+  /// Returns [:true:] if this [Int64] has the same numeric value as the given
+  /// object.  The argument may be an [int] or an [IntX].
+  @override
+  bool operator ==(Object other) => _i == _promote(other);
+
+  @override
+  Int64 abs() => Int64(_i.abs());
+
+  @override
+  Int64 clamp(Object lowerLimit, Object upperLimit) =>
+      Int64(_i.clamp(_promote(lowerLimit), _promote(upperLimit)));
+
+  /// Returns the number of leading zeros in this [Int64] as an [int]
+  /// between 0 and 64.
+  @override
+  int numberOfLeadingZeros() {
+    final high = _i >>> 32;
+    final highLeadingZeroes = u.numberOfLeadingZeros(high);
+    if (highLeadingZeroes == 32) {
+      final low = _i & 0xFFFFFFFF;
+      final lowLeadingZeroes = u.numberOfLeadingZeros(low);
+      return 32 + lowLeadingZeroes;
+    }
+    return highLeadingZeroes;
+  }
+
+  /// Returns the number of trailing zeros in this [Int64] as an [int] between
+  /// 0 and 64.
+  @override
+  int numberOfTrailingZeros() {
+    final low = _i & 0xFFFFFFFF;
+    final lowTrailingZeros = u.numberOfTrailingZeros(low);
+    if (lowTrailingZeros == 32) {
+      final high = _i >>> 32;
+      final highTrailingZeros = u.numberOfTrailingZeros(high);
+      return 32 + highTrailingZeros;
+    }
+    return lowTrailingZeros;
+  }
+
+  @override
+  Int64 toSigned(int width) {
+    if (width < 1 || width > 64) {
+      throw RangeError.range(width, 1, 64);
+    }
+    return Int64(_i.toSigned(width));
+  }
+
+  @override
+  Int64 toUnsigned(int width) {
+    if (width < 0 || width > 64) {
+      throw RangeError.range(width, 0, 64);
+    }
+    return Int64(_i.toUnsigned(width));
+  }
+
+  @override
+  List<int> toBytes() {
+    final result = List<int>.filled(8, 0);
+    result[0] = _i & 0xff;
+    result[1] = (_i >> 8) & 0xff;
+    result[2] = (_i >> 16) & 0xff;
+    result[3] = (_i >> 24) & 0xff;
+    result[4] = (_i >> 32) & 0xff;
+    result[5] = (_i >> 40) & 0xff;
+    result[6] = (_i >> 48) & 0xff;
+    result[7] = (_i >> 56) & 0xff;
+    return result;
+  }
+
+  @override
+  double toDouble() => _i.toDouble();
+
+  @override
+  int toInt() => _i;
+
+  /// Returns an [Int32] containing the low 32 bits of this [Int64].
+  @override
+  Int32 toInt32() => Int32(_i);
+
+  /// Returns `this`.
+  @override
+  Int64 toInt64() => this;
+
+  /// Returns the value of this [Int64] as a decimal [String].
+  @override
+  String toString() => _i.toString();
+
+  @override
+  String toHexString() => toRadixStringUnsigned(16).toUpperCase();
+
+  @override
+  String toRadixString(int radix) => _i.toRadixString(radix);
+
+  String toRadixStringUnsigned(int radix) => _toRadixStringUnsigned(_i, radix);
+
+  /// Returns the digits of `this` when interpreted as an unsigned 64-bit value.
+  String toStringUnsigned() => _toRadixStringUnsigned(_i, 10);
+
+  static String _toRadixStringUnsigned(int value, int radix) =>
+      // low 22 bits, mid 22 bits, high 20 bits
+      u.toRadixStringUnsigned(
+          radix, value & 4194303, (value >> 22) & 4194303, value >>> 44, '');
+}
--- lib/src/utilities.dart
+++ lib/src/utilities.dart
@@ -70,3 +70,120 @@ int bitCount(int i) {
   i += i >> 16;
   return i & 0x0000003F;
 }
+
+String toRadixStringUnsigned(int radix, int d0, int d1, int d2, String sign) {
+  if (d0 == 0 && d1 == 0 && d2 == 0) return '0';
+
+  // Rearrange components into five components where all but the most
+  // significant are 10 bits wide.
+  //
+  //     d4, d3, d4, d1, d0:  24 + 10 + 10 + 10 + 10 bits
+  //
+  // The choice of 10 bits allows a remainder of 20 bits to be scaled by 10
+  // bits and added during division while keeping all intermediate values
+  // within 30 bits (unsigned small integer range for 32 bit implementations
+  // of Dart VM and V8).
+  //
+  //     6  6         5         4         3         2         1
+  //     3210987654321098765432109876543210987654321098765432109876543210
+  //     [--------d2--------][---------d1---------][---------d0---------]
+  //  -->
+  //     [----------d4----------][---d3---][---d2---][---d1---][---d0---]
+
+  int d4 = (d2 << 4) | (d1 >> 18);
+  int d3 = (d1 >> 8) & 0x3ff;
+  d2 = ((d1 << 2) | (d0 >> 20)) & 0x3ff;
+  d1 = (d0 >> 10) & 0x3ff;
+  d0 = d0 & 0x3ff;
+
+  int fatRadix = _fatRadixTable[radix];
+
+  // Generate chunks of digits.  In radix 10, generate 6 digits per chunk.
+  //
+  // This loop generates at most 3 chunks, so we store the chunks in locals
+  // rather than a list.  We are trying to generate digits 20 bits at a time
+  // until we have only 30 bits left.  20 + 20 + 30 > 64 would imply that we
+  // need only two chunks, but radix values 17-19 and 33-36 generate only 15
+  // or 16 bits per iteration, so sometimes the third chunk is needed.
+
+  String chunk1 = '', chunk2 = '', chunk3 = '';
+
+  while (!(d4 == 0 && d3 == 0)) {
+    int q = d4 ~/ fatRadix;
+    int r = d4 - q * fatRadix;
+    d4 = q;
+    d3 += r << 10;
+
+    q = d3 ~/ fatRadix;
+    r = d3 - q * fatRadix;
+    d3 = q;
+    d2 += r << 10;
+
+    q = d2 ~/ fatRadix;
+    r = d2 - q * fatRadix;
+    d2 = q;
+    d1 += r << 10;
+
+    q = d1 ~/ fatRadix;
+    r = d1 - q * fatRadix;
+    d1 = q;
+    d0 += r << 10;
+
+    q = d0 ~/ fatRadix;
+    r = d0 - q * fatRadix;
+    d0 = q;
+
+    assert(chunk3 == '');
+    chunk3 = chunk2;
+    chunk2 = chunk1;
+    // Adding [fatRadix] Forces an extra digit which we discard to get a fixed
+    // width.  E.g.  (1000000 + 123) -> "1000123" -> "000123".  An alternative
+    // would be to pad to the left with zeroes.
+    chunk1 = (fatRadix + r).toRadixString(radix).substring(1);
+  }
+  int residue = (d2 << 20) + (d1 << 10) + d0;
+  String leadingDigits = residue == 0 ? '' : residue.toRadixString(radix);
+  return '$sign$leadingDigits$chunk1$chunk2$chunk3';
+}
+
+// Table of 'fat' radix values.  Each entry for index `i` is the largest power
+// of `i` whose remainder fits in 20 bits.
+const _fatRadixTable = <int>[
+  0,
+  0,
+  2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2,
+  3 * 3 * 3 * 3 * 3 * 3 * 3 * 3 * 3 * 3 * 3 * 3,
+  4 * 4 * 4 * 4 * 4 * 4 * 4 * 4 * 4 * 4,
+  5 * 5 * 5 * 5 * 5 * 5 * 5 * 5,
+  6 * 6 * 6 * 6 * 6 * 6 * 6,
+  7 * 7 * 7 * 7 * 7 * 7 * 7,
+  8 * 8 * 8 * 8 * 8 * 8,
+  9 * 9 * 9 * 9 * 9 * 9,
+  10 * 10 * 10 * 10 * 10 * 10,
+  11 * 11 * 11 * 11 * 11,
+  12 * 12 * 12 * 12 * 12,
+  13 * 13 * 13 * 13 * 13,
+  14 * 14 * 14 * 14 * 14,
+  15 * 15 * 15 * 15 * 15,
+  16 * 16 * 16 * 16 * 16,
+  17 * 17 * 17 * 17,
+  18 * 18 * 18 * 18,
+  19 * 19 * 19 * 19,
+  20 * 20 * 20 * 20,
+  21 * 21 * 21 * 21,
+  22 * 22 * 22 * 22,
+  23 * 23 * 23 * 23,
+  24 * 24 * 24 * 24,
+  25 * 25 * 25 * 25,
+  26 * 26 * 26 * 26,
+  27 * 27 * 27 * 27,
+  28 * 28 * 28 * 28,
+  29 * 29 * 29 * 29,
+  30 * 30 * 30 * 30,
+  31 * 31 * 31 * 31,
+  32 * 32 * 32 * 32,
+  33 * 33 * 33,
+  34 * 34 * 34,
+  35 * 35 * 35,
+  36 * 36 * 36
+];
